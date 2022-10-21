9d35f41237a44c3117796d7e1db2d722bc26f0dc
Moreira-btu <Alex.moreira.btu@hotmail.com>
2022-10-20 22:10:19 -0300

    08

---
+++ js/mm2.js
@@ -0,0 +1,10 @@
+
+  // menu respansivo mobile
+  const btnMobile =document.getElementById('btn-mobile')
+function toggleMenu() {
+ 
+ const nav =document.getElementById('nav')
+ nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click',toggleMenu)
