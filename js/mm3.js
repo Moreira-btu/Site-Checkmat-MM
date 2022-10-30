bf0b8c8f018d13f12824ee345db2935c42b79803
Moreira-btu <Alex.moreira.btu@hotmail.com>
2022-10-30 07:41:10 -0300

    15

--- js/mm3.js
+++ js/mm3.js
@@ -1,12 +1,4 @@
 
-let img = []
-
-img [0] = 'imagens-MM/mm7.jpg'
-img [1] = 'imagens-MM/mm8.jpg'
-img [2] = 'imagens-MM/mm9.jpg'
-img [3] = 'imagens-MM/mm10.jpg'
-
-function mudaFoto (){
- 
-  document.principal.src = img[n]
+ function mudaFoto(foto){
+  document.getElementById('fotos').src=foto
 }
