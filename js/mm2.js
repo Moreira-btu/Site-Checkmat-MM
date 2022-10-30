dbaf3ae82d29a4bbd356a6a27e286b35ff90ce4e
Moreira-btu <Alex.moreira.btu@hotmail.com>
2022-10-29 23:12:29 -0300

    14

--- js/mm2.js
+++ js/mm2.js
@@ -8,3 +8,5 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener('click',toggleMenu)
+
+
---
+++ js/mm3.js
@@ -0,0 +1,12 @@
+
+let img = []
+
+img [0] = 'imagens-MM/mm7.jpg'
+img [1] = 'imagens-MM/mm8.jpg'
+img [2] = 'imagens-MM/mm9.jpg'
+img [3] = 'imagens-MM/mm10.jpg'
+
+function mudaFoto (){
+ 
+  document.principal.src = img[n]
+}
